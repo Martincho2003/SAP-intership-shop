cac02c6330df2ad8f59e529d60c59654ca07d5bd
Martincho2003 <48617764+Martincho2003@users.noreply.github.com>
2024-03-30 23:42:08 +0200

    init project

---
+++ src/test/java/com/example/sap_shop/SapShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sap_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
