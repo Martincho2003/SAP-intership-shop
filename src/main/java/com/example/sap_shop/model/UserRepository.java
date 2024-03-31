0630c03915e42dc2ed90074681792820b5854886
Martincho2003 <48617764+Martincho2003@users.noreply.github.com>
2024-03-31 02:08:46 +0200

    working mysql add
    without any security

--- src/main/java/com/example/sap_shop/model/UserRepository.java
+++ src/main/java/com/example/sap_shop/model/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.sap_shop.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 }
