fd7f6ee15588a196afd3041fe223f4f08586e039
Martincho2003 <48617764+Martincho2003@users.noreply.github.com>
2024-03-31 17:44:11 +0300

    mysql edit
    Added new column to table user
    Changed mysql from locahost to server
    changed create user request

--- src/main/java/com/example/sap_shop/controller/UserController.java
+++ src/main/java/com/example/sap_shop/controller/UserController.java
@@ -3,7 +3,6 @@ package com.example.sap_shop.controller;
 import com.example.sap_shop.model.User;
 import com.example.sap_shop.model.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,15 +13,11 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping(path="/add") // Map ONLY POST Requests
-    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email, @RequestParam String password) {
+    public @ResponseBody String addNewUser (@RequestBody User user) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
 
-        User n = new User();
-        n.setName(name);
-        n.setEmail(email);
-        n.setPassword(String.valueOf(password.hashCode()));
-        userRepository.save(n);
+        userRepository.save(user);
         return "Saved";
     }
 
