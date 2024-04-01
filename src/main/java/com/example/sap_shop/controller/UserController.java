e5eed8816a85570a02219d40969f94f9944ae886
Georgi Stoev <georgi.stoev.2003@gmail.com>
2024-04-02 00:30:00 +0300

    Entities and repositories

--- src/main/java/com/example/sap_shop/controller/UserController.java
+++ src/main/java/com/example/sap_shop/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.example.sap_shop.controller;
 
 import com.example.sap_shop.model.User;
-import com.example.sap_shop.model.UserRepository;
+import com.example.sap_shop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/example/sap_shop/repository/UserRepository.java
+++ src/main/java/com/example/sap_shop/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package com.example.sap_shop.model;
+package com.example.sap_shop.repository;
 
+import com.example.sap_shop.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
